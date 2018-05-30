ffb7ebe5d5d2383fb0a51a4302a957090a74566b
Shivan Modha <shivanmodha@gmail.com>
2018-05-30 09:44:42 -0400

    Webpack and babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+let path = require("path");
+module.exports = {
+    entry: [
+        "src/index.js"
+    ],
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "uikitx.js",
+        libraryTarget: "commonjs2"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, "src"),
+                exclude: /(node_modules|bower_components|build)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["env"]
+                    }
+                }
+            }
+        ]
+    },
+    externals: {
+        "react": "commonjs react"
+    }
+}
